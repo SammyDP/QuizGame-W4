f332e602f8a36fd63e1a7aaed45c8155232baa73
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-07-15 01:30:25 -0600

    link2btn,  JS
    changed high score link to button
    JS - declared elements and added eventlisteners to all buttons

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -13,17 +13,61 @@
 // create list of ranking high scores saved locally, if not reset
 // create go back btn to return home, reset btn to clear cache
 
+// ------------
 // get elements
-const start_btn = document.getElementById(`start_btn`);
-const ansr_btn1 = document.getElementById(`ansr_btn1`);
-const ansr_btn2 = document.getElementById(`ansr_btn2`);
-const ansr_btn3 = document.getElementById(`ansr_btn3`);
-const ansr_btn4 = document.getElementById(`ansr_btn4`);
-const submit_btn = document.getElementById(`submit_btn`);
-const back_btn = document.getElementById(`back_btn`);
-const clear_btn = document.getElementById(`clear_btn`);
+// ------------
+
+// buttons
+const start = document.getElementById(`start-btn`);
+const answer1 = document.getElementById(`ansr_btn1`);
+const answer2 = document.getElementById(`ansr_btn2`);
+const answer3 = document.getElementById(`ansr_btn3`);
+const answer4 = document.getElementById(`ansr_btn4`);
+const submit = document.getElementById(`submit_btn`);
+const back = document.getElementById(`back_btn`);
+const clear = document.getElementById(`clear_btn`);
+const viewHs = document.getElementById(`view-hs`);
+
+// class phases
+const quizIntro = document.querySelector(`.quiz_intro`);
+const questScrn = document.querySelector(`.question_screen`);
+const fnshScrn = document.querySelector(`.finish_screen`);
+const hghScrs = document.querySelector(`.highscores_screen`);
+
+// section elements
+const score = document.querySelector(`.score`);
+const time = document.getElementById(`time`);
+
+// view high scores
+viewHs.addEventListener("click", () => {
+  quizIntro.style.display = "none";
+  score.style.display = "none";
+  viewHs.style.display = "none";
+  fnshScrn.style.display = "none";
+  hghScrs.style.display = "block";
+});
 
 // start quiz
-document.getElementById(`start_btn`).addEventListener(`click`, () => {
-  document.style.setProperty(`--display`, `none`);
+start.addEventListener("click", () => {
+  quizIntro.style.display = "none";
+  questScrn.style.display = "block";
+  viewHs.style.display = "none";
 });
+
+// finish game
+submit.addEventListener("click", () => {
+  fnshScrn.style.display = "none";
+  score.style.display = "none";
+  viewHs.style.display = "none";
+  hghScrs.style.display = "block";
+});
+
+// highscore/game reset
+back.addEventListener("click", () => {
+  hghScrs.style.display = "none";
+  quizIntro.style.display = "block";
+  score.style.display = "flex";
+  viewHs.style.display = "flex";
+});
+
+clear.addEventListener("click", () => {});
