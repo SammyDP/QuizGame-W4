0ae47aa338c79d5f2245abfc242dce8f88eb38d8
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-07-15 02:08:16 -0600

    cleaned notes/psuedo code JS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,9 @@
-// create timer, to countdown
-
-// start button initiates game and changes html screen
-// questions pop up, with answer buttons, when clicked right answers ding and go to next question
-// wrong answers ding/deduct time and go to next question
-
-// once timer ends, then game ends with sound
-
-// change to end screen with high score and intials input
-// need to log game score and initials
-
-// HS link to view high scores
-// create list of ranking high scores saved locally, if not reset
-// create go back btn to return home, reset btn to clear cache
+// -create timer, to countdown
+// -when clicked right answers ding and go to next question
+// -wrong answers ding/deduct time and go to next question
+// -once timer ends, then game ends with sound
+// -final score and intials input, creates list to be saved
+// -reset btn to clear cache
 
 // ------------
 // get elements
@@ -53,6 +45,10 @@ start.addEventListener("click", () => {
   questScrn.style.display = "block";
   viewHs.style.display = "none";
 });
+
+// timer
+
+// game begins
 
 // finish game
 submit.addEventListener("click", () => {
