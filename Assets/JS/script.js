c5a3e63990818ad5ea37211d43349b48533c306f
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-07-14 21:57:38 -0600

    readme, Index, CSS, JS
    readme - questions added
    Index - cleaned up all sections, ready 4 js
    CSS - lined up pages, styled buttons
    JS - linked buttons

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -12,3 +12,18 @@
 // HS link to view high scores
 // create list of ranking high scores saved locally, if not reset
 // create go back btn to return home, reset btn to clear cache
+
+// get elements
+const start_btn = document.getElementById(`start_btn`);
+const ansr_btn1 = document.getElementById(`ansr_btn1`);
+const ansr_btn2 = document.getElementById(`ansr_btn2`);
+const ansr_btn3 = document.getElementById(`ansr_btn3`);
+const ansr_btn4 = document.getElementById(`ansr_btn4`);
+const submit_btn = document.getElementById(`submit_btn`);
+const back_btn = document.getElementById(`back_btn`);
+const clear_btn = document.getElementById(`clear_btn`);
+
+// start quiz
+document.getElementById(`start_btn`).addEventListener(`click`, () => {
+  document.style.setProperty(`--display`, `none`);
+});
